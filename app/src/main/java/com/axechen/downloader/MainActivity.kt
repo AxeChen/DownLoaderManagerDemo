5a8f901193256c74bc36d96bd3213828faba302a
zaifeng <1137366723@qq.com>
2019-08-02 20:55:11 +0800

    Signed-off-by: zaifeng <1137366723@qq.com>

--- app/src/main/java/com/axechen/downloader/MainActivity.kt
+++ app/src/main/java/com/axechen/downloader/MainActivity.kt
@@ -19,12 +19,14 @@ class MainActivity : AppCompatActivity() {
      */
     public fun downLoadApk(view: View) {
         var util = DownLoaderUtil(this)
+        util.downloadApk("百度","http://gdown.baidu.com/data/wisegame/089002f01757d5e4/baidu_49284352.apk",true)
     }
 
     /**
      * 下载文件
      */
     public fun downLoadFile(view: View) {
-
+        var util = DownLoaderUtil(this)
+        util.downloadFile("应用宝","http://gdown.baidu.com/data/wisegame/0995fa2e64afa17e/yingyongbao_7382130.apk","ddd")
     }
 }
